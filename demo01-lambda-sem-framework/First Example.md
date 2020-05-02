1a60940b8fce9c4f00ac66856588bbecef35de84
Alyson Basilio <alyson.basilio@gmail.com>
2020-05-02 19:39:36 -0300

    Demo 01

---
+++ demo01-lambda-sem-framework/First Example.md
@@ -0,0 +1,12 @@
+# First Example: Creating a lambda function
+
+```
+zip function.zip index.js
+docker run --rm -v $(pwd):/app terraform-aws terraform init
+docker run --rm -v $(pwd):/app terraform-aws terraform fmt
+docker run --rm -v $(pwd):/app terraform-aws terraform validate
+docker run --rm -v $(pwd):/app terraform-aws terraform plan
+docker run --rm -v $(pwd):/app terraform-aws terraform apply -auto-approve
+aws lambda invoke --function-name hello_cli --log-type Tail lambda-exec.log
+docker run --rm -v $(pwd):/app terraform-aws terraform destroy -auto-approve
+```
---
+++ demo01-lambda-sem-framework/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/app/credentials"
+}
+
+resource "aws_iam_role" "example_lambda_role" {
+  name = "example_lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "hello_cli" {
+  filename      = "function.zip"
+  function_name = "hello_cli"
+  role          = aws_iam_role.example_lambda_role.arn
+  handler       = "index.handler"
+
+  source_code_hash = filebase64sha256("function.zip")
+
+  runtime = "nodejs12.x"
+}
+ 
